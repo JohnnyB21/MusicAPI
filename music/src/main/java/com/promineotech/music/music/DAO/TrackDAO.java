0a9e8d7f9a7b00e588c159063d814a1001d0306b
Johnny <87739623+JohnnyB21@users.noreply.github.com>
2021-12-03 22:24:16 -0800

    Update TrackDAO.java

--- music/src/main/java/com/promineotech/music/music/DAO/TrackDAO.java
+++ music/src/main/java/com/promineotech/music/music/DAO/TrackDAO.java
@@ -17,7 +17,7 @@ import org.springframework.stereotype.Service;
 public class TrackDAO {
     private static final String HOSTNAME = "jdbc:mysql://localhost:3306/music";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "100Gunter!";
+    private static final String PASSWORD = "123456P";
 
     public static List<Tracks> getTracks(){
         final String sql = "SELECT * FROM tracks";
